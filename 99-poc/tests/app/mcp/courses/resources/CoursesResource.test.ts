6fff14f7fb8600ad023e751a2df202291630a955
Rafa Gómez <rgomezcasas@gmail.com>
2025-08-04 12:11:07 +0200

    feat(poc): test mcp

--- 99-poc/tests/app/mcp/courses/resources/CoursesResource.test.ts
+++ 99-poc/tests/app/mcp/courses/resources/CoursesResource.test.ts
@@ -2,19 +2,17 @@ import "reflect-metadata";
 
 import { CourseRepository } from "../../../../../src/contexts/mooc/courses/domain/CourseRepository";
 import { container } from "../../../../../src/contexts/shared/infrastructure/dependency-injection/diod.config";
+import { PostgresConnection } from "../../../../../src/contexts/shared/infrastructure/postgres/PostgresConnection";
 import { CourseMother } from "../../../../contexts/mooc/courses/domain/CourseMother";
 import { McpClient } from "../../../../contexts/shared/infrastructure/McpClient";
 
 describe("CoursesResource MCP Integration", () => {
 	const mcpClient = new McpClient("./src/app/mcp/server.ts");
 	const courseRepository = container.get(CourseRepository);
+	const connection = container.get(PostgresConnection);
 
 	beforeEach(async () => {
-		const courses = CourseMother.codelyStyleCourses();
-
-		await Promise.all(
-			courses.map((course) => courseRepository.save(course)),
-		);
+		await connection.truncateAll();
 	});
 
 	it("should list courses resource via MCP inspector", async () => {
@@ -22,4 +20,16 @@ describe("CoursesResource MCP Integration", () => {
 
 		expect(resources).toContain("courses://all");
 	}, 10000);
+
+	it("should list empty courses when there are not", async () => {});
+	it("should list existing courses courses", async () => {
+		const course = CourseMother.create();
+		const anotherCourse = CourseMother.create();
+
+		const courses = [course, anotherCourse];
+
+		await Promise.all(
+			courses.map((course) => courseRepository.save(course)),
+		);
+	});
 });
