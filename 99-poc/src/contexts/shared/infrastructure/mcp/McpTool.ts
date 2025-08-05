769c0edfd8b19703f87d99950be02d002b047f2f
Rafa Gómez <rgomezcasas@gmail.com>
2025-08-05 12:04:47 +0200

    feat(poc): improve tool

--- 99-poc/src/contexts/shared/infrastructure/mcp/McpTool.ts
+++ 99-poc/src/contexts/shared/infrastructure/mcp/McpTool.ts
@@ -1,14 +1,9 @@
+import { McpToolResponse } from "./McpToolResponse";
+
 export interface McpTool {
 	name: string;
 	title: string;
 	description: string;
 	inputSchema: object;
-	handler: () => Promise<{
-		content: Array<{
-			type: "text";
-			text: string;
-		}>;
-		structuredContent?: Record<string, unknown>;
-		isError?: boolean;
-	}>;
+	handler: () => Promise<McpToolResponse>;
 }
