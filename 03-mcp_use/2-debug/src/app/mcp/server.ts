1d8dc6ac70f8b548e6752fb13fafd6bb1ba9cb5b
Rafa Gómez <rgomezcasas@gmail.com>
2025-08-13 19:26:20 +0200

    feat(03-2): add example

--- 03-mcp_use/2-debug/src/app/mcp/server.ts
+++ 03-mcp_use/2-debug/src/app/mcp/server.ts
@@ -29,7 +29,7 @@ server.registerTool(
 		description: "View the disk space in G",
 	},
 	() => {
-		const stdout = execSync("df -h / | /usr/bin/awk 'NR==2 {print $4}'", {
+		const stdout = execSync("df -h / | /usr/bin/awk 'NR==2 {print $3}'", {
 			encoding: "utf8",
 		});
 
