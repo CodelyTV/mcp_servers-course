39530e48cbd9c8c8447608f60d955a40f692594f
Rafa Gómez <rgomezcasas@gmail.com>
2025-08-18 12:14:08 +0200

    feat(06-1): prepare to extract test library

--- 06-mpc_http/1-http_transport/tests/app/mcp/courses/resources/CourseResourceTemplate.test.ts
+++ 06-mpc_http/1-http_transport/tests/app/mcp/courses/resources/CourseResourceTemplate.test.ts
@@ -3,12 +3,11 @@ import "reflect-metadata";
 import { CourseRepository } from "../../../../../src/contexts/mooc/courses/domain/CourseRepository";
 import { container } from "../../../../../src/contexts/shared/infrastructure/dependency-injection/diod.config";
 import { PostgresConnection } from "../../../../../src/contexts/shared/infrastructure/postgres/PostgresConnection";
-import { CourseIdMother } from "../../../../contexts/mooc/courses/domain/CourseIdMother";
 import { CourseMother } from "../../../../contexts/mooc/courses/domain/CourseMother";
 import { McpTestClient } from "../../../../contexts/shared/infrastructure/mcp-inspector-cli-client/McpTestClient";
 
 describe("CourseResourceTemplate should", () => {
-	const mcpClient = new McpTestClient([
+	const mcpClient = new McpTestClient("stdio", [
 		"npx",
 		"ts-node",
 		"./src/app/mcp/server.ts",
@@ -16,11 +15,16 @@ describe("CourseResourceTemplate should", () => {
 	const courseRepository = container.get(CourseRepository);
 	const connection = container.get(PostgresConnection);
 
+	beforeAll(async () => {
+		await mcpClient.connect();
+	});
+
 	beforeEach(async () => {
 		await connection.truncateAll();
 	});
 
 	afterAll(async () => {
+		await mcpClient.disconnect();
 		await connection.end();
 	});
 
@@ -30,28 +34,8 @@ describe("CourseResourceTemplate should", () => {
 		expect(resourceTemplates.uris()).toContain("courses://{id}");
 	});
 
-	it("return bad request error when course id is invalid", async () => {
-		const invalidId = CourseIdMother.invalid();
-
-		await expect(
-			mcpClient.readResource(`courses://${invalidId}`),
-		).rejects.toThrow(
-			`Process exited with code 1: Failed to read resource courses://${invalidId}: MCP error -32000: The id <${invalidId}> is not a valid nano id`,
-		);
-	});
-
-	it("return not found error when course is not found", async () => {
-		const nonExistingCourseId = CourseIdMother.create().value;
-
-		await expect(
-			mcpClient.readResource(`courses://${nonExistingCourseId}`),
-		).rejects.toThrow(
-			`Process exited with code 1: Failed to read resource courses://${nonExistingCourseId}: MCP error -32002: The course <${nonExistingCourseId}> has not been found`,
-		);
-	});
-
 	it("return course details when course exists", async () => {
-		const course = CourseMother.createdToday();
+		const course = CourseMother.create();
 
 		await courseRepository.save(course);
 
@@ -68,5 +52,40 @@ describe("CourseResourceTemplate should", () => {
 				},
 			],
 		});
+	});
+
+	it("list all available courses as resources", async () => {
+		const course = CourseMother.create();
+		const anotherCourse = CourseMother.create();
+
+		await courseRepository.save(course);
+		await courseRepository.save(anotherCourse);
+
+		const response = await mcpClient.listResources();
+
+		expect(response.uris()).toEqual(
+			expect.arrayContaining([
+				`course://${course.id.value}`,
+				`course://${anotherCourse.id.value}`,
+			]),
+		);
+	});
+
+	it("complete the id param", async () => {
+		const testCourse = CourseMother.createdToday({ id: "t3st" });
+		const teatCourse = CourseMother.createdYesterday({ id: "t3at" });
+		const codeCourse = CourseMother.create({ id: "c0d3" });
+
+		await courseRepository.save(testCourse);
+		await courseRepository.save(teatCourse);
+		await courseRepository.save(codeCourse);
+
+		const response = await mcpClient.completeResourceTemplateParam(
+			"courses://{id}",
+			"id",
+			"t3",
+		);
+
+		expect(response).toEqual(["t3st", "t3at"]);
 	});
 });
